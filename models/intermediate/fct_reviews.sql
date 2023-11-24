c6291b19140e1041c05fa8568912009ea4a70470
filipebalseiro <filipe_balseiro@hakkoda.io>
2023-11-24 11:00:46 +0000

    Added surrogate key

--- models/intermediate/fct_reviews.sql
+++ models/intermediate/fct_reviews.sql
@@ -1,6 +1,8 @@
 {{ config(materialized="incremental", on_schema_change="fail") }}
 with stg_reviews as (select * from {{ ref("stg_reviews") }})
-select *
+select
+    {{ dbt_utils.generate_surrogate_key(['listing_id', 'review_date','reviewer_name', 'review_text', 'review_sentiment']) }} AS review_id,
+    *
 from stg_reviews
 where
     review_text is not null
